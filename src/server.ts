0c5eb49fc8dc39b925437e32740c0850e81771c5
sohag55rana <sohag55rana@gmail.com>
2026-06-20 10:56:27 +0600

    now Prepare backend for in  deployment

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
-// import { PrismaClient } from '@prisma/client';
-// import dotenv from "dotenv";
 import app from "./app.js";
 import { prisma } from "./lib/prisma.js";
-
-// dotenv.config();
-
-// export const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 5000;
 
 async function server() {
   try {
-    await prisma.$disconnect();
+    await prisma.$connect();
 
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT} continue`);
+      console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Database connection failed:", error);
