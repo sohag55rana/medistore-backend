3da6b08bd651afa4e4ea7d8d75618e158c5a1880
sohag55rana <sohag55rana@gmail.com>
2026-06-20 12:24:15 +0600

    now Fix cors link  for production

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://medistore-frontend-rose.vercel.app/",
+      "https://medistore-frontend-rose.vercel.app",
     ],
     credentials: true,
   }),
