67a1f789f5fbfbb960b1434802df015458730f88
sohag55rana <sohag55rana@gmail.com>
2026-06-20 13:05:29 +0600

    now Fix cors link  for a  production

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -87,9 +87,9 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({
